e6970c672ae0a1a29813c34a8b87e585d5e1b9cb
Nick Weiland <nickweiland@gmail.com>
2016-02-22 14:25:09 -0700

    Add flash validation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
   def index
+    session[:hit_counter] = session[:hit_counter].to_i + 1
+    if session[:hit_counter].to_i.even?
+      flash.now[:alert] = "Hit counter is even."
+    else
+      flash.now[:message] = "Hit counter is odd."
+    end
     @items = Item.all
   end
 
